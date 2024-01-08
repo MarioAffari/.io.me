1be0c24b893e58bc07710868e0a47216dfde5cee
MarioAffari <156019971+MarioAffari@users.noreply.github.com>
2024-01-08 21:39:04 +0100

    Create Costants.ts

---
+++ src/Costants.ts
@@ -0,0 +1,8 @@
+export const Commands: { [key: string]: string[] } = {
+    '/start': ['user', 'admin'],
+    '/help': ['user', 'admin'],
+    '/delete': ['admin'],
+    '/ban': ['admin'],
+    '/unban': ['admin'],
+    // Add more commands as needed
+  };
